e13882991d6ec7c2079725ddcf8ffa8199c2bd1a
xi_jjun <rlawowns97@naver.com>
2022-03-07 14:55:21 +0900

    git init

---
+++ src/main/java/project/board/springoauth2project/SpringOauth2ProjectApplication.java
@@ -0,0 +1,13 @@
+package project.board.springoauth2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/board/springoauth2project/SpringOauth2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.board.springoauth2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
